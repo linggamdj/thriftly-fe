58212a72e02ab1ca95e414cbd017bbe52dc21a07
Lingga <linggaadiatmdj@gmail.com>
2023-06-10 12:14:50 +0700

    update API

--- src/helpers/AxiosHelper.js
+++ src/helpers/AxiosHelper.js
@@ -1,4 +1,5 @@
 // const API_URL = "http://localhost:3000/api";
-const API_URL = "https://thriftly.glitch.me/api";
+// const API_URL = "https://thriftly.glitch.me/api";
+const API_URL = "https://thriftly.fly.dev/api";
 
 export { API_URL };
--- src/helpers/Constants.js
+++ src/helpers/Constants.js
@@ -1,4 +1,5 @@
 // const APP_URL = "http://localhost:3000/";
-const APP_URL = "https://thriftly.glitch.me/";
+// const APP_URL = "https://thriftly.glitch.me/";
+const APP_URL = "https://thriftly.fly.dev/";
 
 export { APP_URL };
